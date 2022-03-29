c7fc5255850f53649cc6d3236de9e2919da73162
Castle <castlerock88@protonmail.com>
2022-03-28 23:13:06 -0400

    wrong connection type

--- game.js
+++ game.js
@@ -220,23 +220,14 @@ playas.forEach((p) => {
 });
 
 const client = new tmi.Client({
-  options: { debug: true, messagesLogLevel: "info" },
-  connection: {
-    reconnect: true,
-    secure: true,
-  },
-  identity: {
-    username: "bot-name",
-    password: "oauth:my-bot-token",
-  },
-  channels: ["my-channel"],
+  channels: ["whoa_dood"],
 });
 
-client.connect().catch(console.error);
+client.connect();
 
 client.on("message", (channel, tags, message, self) => {
-  // if (self) return;
-  if (message.toLowerCase() === "!hello") {
-    console.log(channel, `@${tags.username}, heya!`);
+  // "Alca: Hello, World!"
+  if (message.startsWith("!nade")) {
+    console.log(`${tags["display-name"]}: ${message}`);
   }
 });
